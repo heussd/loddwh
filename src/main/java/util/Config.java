44ad1ebaed993953990057168d92a4a2ed7cd30f
Timm Heuss <Timm.Heuss@web.de>
2015-09-18 16:41:56 +0200

    Made config Multi-OS-compatible

--- src/main/java/util/Config.java
+++ src/main/java/util/Config.java
@@ -8,8 +8,7 @@ import freemarker.template.DefaultObjectWrapper;
 public class Config extends DefaultObjectWrapper {
 	public final static String WHERE_THE_FILES_AT = "/Users/th/Ph.D./hebisdumps/noHeader/";
 	public final static String WHERE_THE_RESULTS_AT = "";
-	
-	
+
 	public final static String USER = "bench";
 	public static final String PASSWORD = "";
 	public final static String DATABASE = "loddwhbench";
@@ -19,8 +18,9 @@ public class Config extends DefaultObjectWrapper {
 	public static final String HOST_POSTGRES = "localhost";
 
 	public static final String NEWCOLUMN = "newcolumn";
-	
-	
-	public static final String VIRTUOSO_ACCESSIBLE_DIRECTORY = "C:\\RDSTUDIES\\db\\virtuoso-opensource";
-	public static final String VIRTUOSO_ACCESSIBLE_DIRECTORY_RELATIVE_TO_VIRTUOSO_T_EXE = "..";	
+
+	public static final String VIRTUOSO_ACCESSIBLE_DIRECTORY = System.getProperty("os.name").contains("OS X") ? "/usr/local/virtuoso-opensource/share/virtuoso/vad/"
+			: "C:\\RDSTUDIES\\db\\virtuoso-opensource";
+	public static final String VIRTUOSO_ACCESSIBLE_DIRECTORY_RELATIVE_TO_VIRTUOSO_T_EXE = System.getProperty("os.name").contains("OS X")
+			? "/usr/local/virtuoso-opensource/share/virtuoso/vad/" : "..";
 }
