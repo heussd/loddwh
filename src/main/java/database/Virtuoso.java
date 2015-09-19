9ae17360e54f00bc6d43ae69aad48bc091d9d69c
Timm Heuss <Timm.Heuss@web.de>
2015-09-19 11:36:59 +0200

    Harmonised Virtuoso query execution mechanism
    Also, first benchmark run.

--- src/main/java/database/Virtuoso.java
+++ src/main/java/database/Virtuoso.java
@@ -1,19 +1,15 @@
 package database;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
 import java.util.zip.GZIPInputStream;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.NotImplementedException;
-
-import util.Codes;
 import util.Config;
 import util.Dataset;
 import util.QueryResult;
@@ -24,23 +20,30 @@ import util.Templates;
 public class Virtuoso implements Database {
 
 	public static void main(String[] args) throws Throwable {
-		Virtuoso testVirtuoso = new Virtuoso("hebis_1000_test");
-		for (QueryScenario qs : QueryScenario.values()) {
-			System.out.println(String.format(testVirtuoso.templates.resolve(qs), "GRAPH_ID"));
-		}
+		Virtuoso testVirtuoso = new Virtuoso();
+		testVirtuoso.setUp();
+		testVirtuoso.load(Dataset.hebis_1000_records);
+
+		QueryScenario queryScenario = QueryScenario.ENTITY_RETRIEVAL_BY_ID_100_ENTITIES;
+		testVirtuoso.prepare(queryScenario);
+		testVirtuoso.query(queryScenario);
+
 	}
 
-	// TODO graphId irrelevant, immer denselben Graphen löschen und neu aufbauen, überall anpassen, in query und den templates
+	// TODO graphId irrelevant, immer denselben Graphen lï¿½schen und neu
+	// aufbauen, ï¿½berall anpassen, in query und den templates
 	private String graphId, vAD, vADRtVE;
-	private Connection c;
+	private Connection connection;
 	private Statement stmt;
 	private Templates templates;
+	private ArrayList<PreparedStatement> scenarioStatements;
+	private QueryScenario queryScenario;
 
-	public Virtuoso(String identifier) {
-		graphId = identifier;
+	public Virtuoso() {
+		graphId = Config.DATABASE;
 		vAD = Config.VIRTUOSO_ACCESSIBLE_DIRECTORY;
 		vADRtVE = Config.VIRTUOSO_ACCESSIBLE_DIRECTORY_RELATIVE_TO_VIRTUOSO_T_EXE;
-		
+
 		templates = new Templates("virtuoso", ".sql");
 	}
 
@@ -51,14 +54,13 @@ public class Virtuoso implements Database {
 
 	@Override
 	public String getVersion() {
-		return "7.2.1";
+		return "7.2.1 / Virtuoso JDBC 4.1";
 	}
 
 	@Override
 	public void setUp() throws Exception {
-		c = DriverManager.getConnection("jdbc:virtuoso://127.0.0.1", "dba",
-				"dba");
-		stmt = c.createStatement();
+		connection = DriverManager.getConnection("jdbc:virtuoso://127.0.0.1", "dba", "dba");
+		stmt = connection.createStatement();
 
 		// Drop auf evtl. alten Identifier
 		stmt.execute(String.format("SPARQL CLEAR GRAPH <%s>", graphId));
@@ -72,8 +74,7 @@ public class Virtuoso implements Database {
 		// 1.
 		byte[] buffer = new byte[1024];
 		GZIPInputStream gzis = new GZIPInputStream(new FileInputStream(dataset.string));
-		FileOutputStream out = new FileOutputStream(String.format("%s//%s",
-				vAD, graphId));
+		FileOutputStream out = new FileOutputStream(String.format("%s//%s", vAD, graphId));
 		int len;
 		while ((len = gzis.read(buffer)) > 0) {
 			out.write(buffer, 0, len);
@@ -82,21 +83,99 @@ public class Virtuoso implements Database {
 		out.close();
 
 		// 2.
-		stmt.executeQuery(String
-				.format("DB.DBA.RDF_LOAD_RDFXML_MT (file_to_string_output ('%s/%s'), '', '%s')",
-						vADRtVE, graphId, graphId));
+		stmt.executeQuery(String.format("DB.DBA.RDF_LOAD_RDFXML_MT (file_to_string_output ('%s/%s'), '', '%s')", vADRtVE, graphId, graphId));
 	}
 
 	@Override
 	public void prepare(QueryScenario queryScenario) throws Exception {
+		if (scenarioStatements == null)
+			scenarioStatements = new ArrayList<>();
+
+		scenarioStatements.clear();
+
+		scenarioStatements.add(connection.prepareStatement("sparql " + String.format(templates.resolve(queryScenario), graphId)));
+		this.queryScenario = queryScenario;
 	}
 
 	@Override
 	public QueryResult query(QueryScenario queryScenario) throws Exception {
-		
-		stmt.execute(String.format(templates.resolve(queryScenario), graphId));
-		
-		// TODO 
+		// scenarioStatements.add(connection.prepareStatement(templates.resolve(queryScenario)));
+
+		// stmt.executeQuery(stmt.execute(String.format(templates.resolve(queryScenario),
+		// graphId))
+		for (PreparedStatement preparedStatement : scenarioStatements) {
+			ResultSet resultSet;
+//			System.out.println(preparedStatement);
+			switch (queryScenario.queryResultType) {
+			case GRAPH:
+				resultSet = preparedStatement.executeQuery();
+				while (resultSet.next()) {
+//					System.out.println(resultSet.getString(1));
+					// switch (resultSet.getMetaData().getColumnCount()) {
+					// case 3:
+					// queryResult.push(resultSet.getString(1),
+					// resultSet.getString(2), resultSet.getString(3));
+					// break;
+					// case 5:
+					// queryResult.push(resultSet.getString(1),
+					// resultSet.getString(2), resultSet.getString(3),
+					// resultSet.getString(4), resultSet.getString(5));
+					// break;
+					// default:
+					// throw new RuntimeException("Cannot parse " +
+					// resultSet.getMetaData().getColumnCount() + " columns in
+					// result set");
+					// }
+				}
+				break;
+			case TWO_COLUMNS:
+				resultSet = preparedStatement.executeQuery();
+				while (resultSet.next()) {
+					// queryResult.push(resultSet.getString(1),
+					// resultSet.getString(2));
+					String s = "";
+					for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
+						s += resultSet.getMetaData().getColumnLabel(i) + ": " + resultSet.getString(i) + " ";
+					}
+//					System.out.println(s);
+				}
+				break;
+			case COMPLETE_ENTITIES:
+				resultSet = preparedStatement.executeQuery();
+
+				while (resultSet.next()) {
+					String s = "";
+					for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
+						s += resultSet.getMetaData().getColumnLabel(i) + ": " + resultSet.getString(i) + " ";
+					}
+					// System.out.println(s);
+					// System.out.println(resultSet.getString(1) + " " +
+					// resultSet.getString(2) + " " + resultSet.getString(3) + "
+					// " + resultSet.getString(4));
+					// DataObject dataObject = new DataObject();
+					// for (Codes code : Codes.values()) {
+					// if (code.IS_MULTIPLE) {
+					// for (String value : (String[])
+					// resultSet.getArray(code.ordinal() + 1).getArray()) {
+					// dataObject.putMultiple(code, value);
+					// }
+					//
+					// } else {
+					// dataObject.set(code, resultSet.getString(code.ordinal() +
+					// 1));
+					// }
+					// }
+					// queryResult.push(dataObject);
+				}
+				break;
+			case NONE:
+			default:
+				// This will only work for non-readOnly-scenarios!
+				preparedStatement.executeUpdate();
+				break;
+			}
+		}
+		// TODO
 		return new QueryResult(Type.NONE);
 	}
 
@@ -107,9 +186,7 @@ public class Virtuoso implements Database {
 
 	@Override
 	public String toString() {
-		return "Virtuoso [getName()=" + getName() + ", getVersion()="
-				+ getVersion() + "]";
+		return "Virtuoso [getName()=" + getName() + ", getVersion()=" + getVersion() + "]";
 	}
 
-	
 }
