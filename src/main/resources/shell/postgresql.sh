802953ed03c889f8ac1e1a3b6d55d6140278ee13
Timm Heuss <Timm.Heuss@web.de>
2015-11-05 11:35:58 +0100

    Config for 16GB RAM

--- src/main/resources/shell/postgresql.sh
+++ src/main/resources/shell/postgresql.sh
@@ -2,6 +2,9 @@
 
 rm -R /usr/local/var/loddwhbench
 initdb -E UTF8 -D /usr/local/var/loddwhbench
+
+pgtune -i /usr/local/var/loddwhbench/postgresql.conf -o /usr/local/var/loddwhbench/postgresql.conf -M 17179869184
+
 pg_ctl -D /usr/local/var/loddwhbench start
 #read -p "Press [Enter] to stop PostgreSQL..."
 #pg_ctl -D /usr/local/var/loddwhbench stop -m immediate
